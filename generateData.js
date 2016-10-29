079579ceff24f656b06acffbbe416d7b6635733e
portalSDK <istptadm@uwaterloo.ca>
2016-10-29 12:39:29 -0400

    Created new file: generateData.js

---
+++ generateData.js
@@ -0,0 +1,15 @@
+function createData(){
+ for (var i=0;i<10;i++){
+    var name = 'Doctor'+i;
+    console.log(name);
+    for(var j=0;j<10;j++){
+    	   
+    }
+     
+     
+ }  
+    
+    
+}
+
+createData();

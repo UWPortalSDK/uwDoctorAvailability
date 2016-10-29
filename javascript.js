07a5cd3830ead4de0d5039c1d052c47aab35a945
jerryguo95 <jerry-guo@live.ca>
2016-10-29 14:21:27 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -24,5 +24,60 @@ angular.module('portalApp')
     //});
     return true;
   };
+    //calender stuff
+    $scope.inlineOptions = {
+      customClass: getDayClass,
+      minDate: new Date(),
+      showWeeks: true
+    };
+
+    $scope.dateOptions = {
+      dateDisabled: disabled,
+      formatYear: 'yy',
+      maxDate: new Date(2020, 5, 22),
+      minDate: new Date(),
+      startingDay: 1
+    };
+    
+    function disabled(data) {
+      var date = data.date,
+        mode = data.mode;
+      return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+    }
+    $scope.toggleMin = function() {
+      $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+      $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+    };
+    
+  $scope.toggleMin();
+  function getDayClass(data) {
+      var date = data.date,
+      mode = data.mode;
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+        for (var i = 0; i < $scope.events.length; i++) {
+          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+          if (dayToCheck === currentDay) {
+            return $scope.events[i].status;
+          }
+        }
+      }
+
+      return '';
+    }
+    $scope.selectedDate = new Date();
+
+    //$scope.selectedDate.setHours(1);
+    //$scope.selectedDate.setMinutes(20);
+    $scope.openCalender = function() {
+      $scope.CalPopup.opened = true;
+    };
+    
+    $scope.CalPopup = {
+      opened: false
+    };
+    
 }]);
 

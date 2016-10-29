311fe99be75172f48c59c879cc17b32791c9ad91
Charles Qi <ccqi@uwaterloo.ca>
2016-10-29 12:53:28 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -33,7 +33,9 @@ angular.module('portalApp')
 		}
 	];
 	
-    $
+    $scope.filter = function() {
+        console.log('here');
+    }
     
 	// Show main view in the first column as soon as controller loads
 	$scope.portalHelpers.showView('uwDoctorAvailabilityMain.html', 1);

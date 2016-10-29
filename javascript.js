02c9a4cc31c195adcbabb64df9a3ec23962cbe77
R.ichard.Di <richarddiweizhe@gmail.com>
2016-10-29 13:55:04 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -1,67 +1,10 @@
 
-var import = ["generateData.js"];
+var imports = ["generateData.js"];
 angular.module('portalApp')
 .controller('uwDoctorAvailabilityCtrl', ['$scope', function ($scope) {
   
   // mock data
-  $scope.items = {value: [
-    {
-      name:'Richard',
-      availability: [
-                {
-                    start: '10-03-2016 8:30AM',
-                    end: '10-05-2016 8:45AM'
-                },
-                {
-                    start: '10-04-2016 9:30AM',
-                    end: '10-05-2016 8:45AM'
-                }
-            ],
-      speciality: 'Nurse'
-    },
-    {
-      name:'Charles',
-      availability: [
-                {
-                    start: '10-06-2016 1:30PM',
-                    end: '10-06-2016 1:45PM'
-                },
-                {
-                    start: '10-04-2016 2:30PM',
-                    end: '10-05-2016 2:45PM'
-                }
-            ],
-      speciality: 'GP'
-    },
-        {
-      name:'Emma',
-      availability: [
-                {
-                    start: '10-03-2016 8:30AM',
-                    end: '10-05-2016 8:45AM'
-                },
-                {
-                    start: '10-04-2016 9:30AM',
-                    end: '10-05-2016 8:45AM'
-                }
-            ],
-      speciality: 'Surgeon'
-    },
-        {
-      name:'Jaden',
-      availability: [
-                {
-                    start: '10-03-2016 8:30AM',
-                    end: '10-05-2016 8:45AM'
-                },
-                {
-                    start: '10-04-2016 9:30AM',
-                    end: '10-05-2016 8:45AM'
-                }
-            ],
-      speciality: 'Internal Medicine'
-    }
-  ]};
+  $scope.items = {value: createData()};
     
   // Show main view in the first column as soon as controller loads
   $scope.portalHelpers.showView('uwDoctorAvailabilityMain.html', 1);

162785f36ef32d2e525e9c55c92d4a8b77be6b29
R.ichard.Di <richarddiweizhe@gmail.com>
2016-10-29 12:52:25 -0400

    Updated user branch from Portal workspace (generateData.js)

--- generateData.js
+++ generateData.js
@@ -1,15 +1,24 @@
 function createData(){
+ var mockData = [];
  for (var i=0;i<10;i++){
     var name = 'Doctor'+i;
-    console.log(name);
+    var times = [];
     for(var j=0;j<10;j++){
-    	   
+        var start = moment([2016, 10, 19+i, 10+j, 15]).format();
+        var end = moment([2016, 10, 19+i, 10+j, 55]).format();
+        times.push({'start':start,'end':end});     
     }
+    var obj = {
+			'name': name,
+			'availability':times,
+			'speciality': 'Software'
+	};
+    mockData.push(obj);
      
      
  }  
+ return mockData;
     
     
 }
 
-createData();
